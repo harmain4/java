91c7787614d993f0ffe4915056de2952d4c81b03
harmain4 <affanmohammad669@gmail.com>
2024-10-12 20:51:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+// chapter 1
+// question1
+// alert("welcom");
+
+// question2
+// alert("please! enter avalid password")
+
+// question 3
+// alert("welcome to js land \n happy coding")
+
+// question 4
+// alert("welcome to js land");
+// alert("happy coding!");
+
+// question 5
+// alert("Hello....i run js through my web browser console")
+
+//    chapter 2
+// question 1
+// let username="azra";
+
+// question 2
+// let myname ="azra bano"
+// console.log(myname);
+
+// 'question 3'
+// let massage ='good morning';
+// console.log(massage);
+
+// question 4
+// let = "studient bio data"
+// let sname ="azra";
+// let sage ="20";
+// alert(`studient name is ${(sname)} and studient age is ${sage}`);
+
+// question 5
+// alert("pizza \n pizza \n pizza \n pizza \n pizza")
+
+// questin 6
+// let email="affanmohammad669@gmail.com";
+// alert("my email is " + email)
+
+// question 6
+// let book =" A smarter way to learn java script "
+// alert(" i am trying to leearn" + book)
+
+// question 7
+// document.write("yeh! i can write HTML content through java script");
+// console.log(document.write);
+
+// question 8
+// let image ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(image);
+
+    // chapter 3
+
+ // question 1
+//  let age =" 25 years old";
+//  alert("i am" + age);
+
+// question 2
+// var visitedtimes = 20;
+// alert("you visited this site " + visitedtimes + " times");
+
+// question 3
+// var birhtYear = 2010;
+// document.write("my birth year is " + birhtYear);
+// document.write("<br>")
+//  document.write ("data type of my ddecleared veriable is number")
+
+//   question 4
+// var viitorsName ="jhon doe";
+// var productTitle = "t shirts";
+// var quantity = 10;
+// document.write(`${visitorName}  order ${productTitle} shits ${quantity}`);s
+
+// chapter 4
+// question 1
+// var a ="spider man";
+// var b ="jasmeen";
+// var c =5;
+// console.log( a,b,c);
+
+// question 2
+
+// legal variables
+// var myName ="azra";
+// var my_name ="azra";
+// var MyName ="azra";
+//  var myage = 25;
+//  let salesQuqntity = 2.5;
+ 
+//  console.log(myName,my_name, MyName,myage,salesQuqntity)
+
+// illigal veriables
+        
+// 1. let 123x = "Variable names cannot start with numbers";
+// 2. var @symbol =" Variable names cannot contain special characters except for underscore (_)";
+// 3. const my-variable = "aria Vble names cannot contain hyphens";
+// 4. let my variable = "Variable names cannot contain spaces";
+// 5. var #hash =  "Variable names cannot start with special characters except for underscore (_)";
+
+// question 3
+
+// question 3 a
+// document.write( `<h1>  Rules for naming variables rules </h1>`);
+
+// question 3 b
+// document.write("variables name can only contain Lettars, Numbers, dollars signs and underscorsec <br> veriable must begun with a_ ,_,_ for example name or name.<br>vriables are _ case.<br> variables name should not be JS." )
+
+
+// chapter 5
+// question 1 
+// let num1 = 3;
+// let num2 = 5;
+// document.write(`sum of ${num1}and + ${num2} 1s`);
